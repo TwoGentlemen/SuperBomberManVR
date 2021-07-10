9df5fddad984b9ccea572625540ad773015b7acf
Ivan(Noyt) <cnyazev.ivan@yandex.ru>
2021-07-10 17:32:25 +0300

    Настроил проект под билд

--- Assets/Scripts/PlayerControllVR.cs
+++ Assets/Scripts/PlayerControllVR.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerControllVR : MonoBehaviour
 {
+
     [SerializeField] private ActionBasedContinuousMoveProvider moveControll;
+    [SerializeField] private InputActionProperty shootAction;
+
+    [SerializeField] private UnityEvent onPressShoot;
+    private void Start()
+    {
+        shootAction.action.performed += ShootAction;
+    }
+
+    private void ShootAction(InputAction.CallbackContext obj)
+    {
+        onPressShoot?.Invoke();
+    }
 
     public void AddSpeed()
     {
         moveControll.moveSpeed+=0.2f;
-        Debug.Log("Добавлена скорость");
+        Debug.Log("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
     }
    
 }
