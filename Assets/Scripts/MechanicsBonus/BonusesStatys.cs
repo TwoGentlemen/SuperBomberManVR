ccf98b17385c9ae188d200d659765704c6541adb
IvanT771 <cnyazev.ivan@yandex.ru>
2021-07-13 14:17:02 +0300

    Создал второй уровень

---
+++ Assets/Scripts/MechanicsBonus/BonusesStatys.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Bonuses",fileName = "BonusStratys")]
+public class BonusesStatys : ScriptableObject
+{
+    public int radiusBomb = 1;
+    public int countBomb = 1;
+    public int hp;
+
+}
